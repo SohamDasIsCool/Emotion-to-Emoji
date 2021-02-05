60f96b8ea6262a7144799032812538a89646d9f8
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2021-02-05 18:51:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function gotResult(error, results) {
     else {
     console.log(results);
     document.getElementById("result_emotion_name").innerHTML=results[0].label;
-    document.getElementById("result_emotion_name").innerHTML=results[1].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
     prediction_1=results[0].label;
     prediction_2=results[1].label;
     speak();
